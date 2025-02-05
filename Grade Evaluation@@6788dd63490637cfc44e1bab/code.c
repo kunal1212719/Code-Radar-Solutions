36eb6f60f52ddaeec42842d9d1a7cc56cb175209
kunal1212719 <kunal1212.becse24@chitkara.edu.in>
2025-02-05 18:27:33 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    char i;
+    if(i=='A')
+    {
+        printf("Excellent");
+    }
+    else if(i=='B')
+    {
+        printf("Good");
+    }
+    else if(i=='C')
+    {
+        printf("Average");
+    }
+    else if(i=='D')
+    {
+        printf("Below Average");
+    }
+    else if(i=='f')
+    {
+        printf("Fail");
+    }
+    else
+    {
+        printf("Invalid grade");
+    }
+    return 0;
+}
